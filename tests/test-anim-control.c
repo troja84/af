bf55d40bf513ae7ed06bbd9bb590fb0a23558267
Hagen Schink <hagen@imendio.com>
2008-10-25 13:55:21 +0200

    Quit main loop when closing the window.

--- tests/test-anim-control.c
+++ tests/test-anim-control.c
@@ -53,6 +53,9 @@ main (int argc, char *argv[])
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_window_set_default_size (GTK_WINDOW (window), 400, 300);
 
+  g_signal_connect (window, "destroy",
+                    G_CALLBACK (gtk_main_quit), NULL);
+
   box = gtk_vbox_new (FALSE, 6);
 
   label = gtk_label_new ("Sliff sloff");
