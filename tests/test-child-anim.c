b668ee05335cf9bed967e32b054932ae9eba9a6b
Carlos Garnacho <carlos@imendio.com>
2008-10-25 11:26:27 +0200

    Add missing test-child-anim test.

---
+++ tests/test-child-anim.c
@@ -0,0 +1,36 @@
+#include <gtk/gtk.h>
+#include <af/af-animator.h>
+
+int
+main (int argc, char *argv[])
+{
+  GtkWidget *window, *fixed, *label;
+  guint id;
+
+  gtk_init (&argc, &argv);
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_default_size (GTK_WINDOW (window), 300, 300);
+  g_signal_connect (window, "destroy",
+                    G_CALLBACK (gtk_main_quit), NULL);
+
+  fixed = gtk_fixed_new ();
+
+  label = gtk_label_new ("Sliff Sloff");
+  gtk_fixed_put (GTK_FIXED (fixed), label, 0, 0);
+
+  gtk_container_add (GTK_CONTAINER (window), fixed);
+
+  id = af_animator_child_tween (GTK_CONTAINER (fixed),
+                                label,
+                                1000,
+                                "x", 200,
+                                "y", 200,
+                                NULL);
+
+  gtk_widget_show_all (window);
+
+  gtk_main ();
+
+  return 0;
+}
